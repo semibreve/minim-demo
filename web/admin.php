d95e5bd67ffc6a0cd8800ba1e905156a9160b234
Saul Johnson <saul.a.johnson@gmail.com>
2016-09-13 14:01:20 +0100

    Add logged in page

--- web/admin.php
+++ web/admin.php
@@ -13,3 +13,52 @@ if (!$auth->isAuthenticated()) {
 }
 
 ?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <link rel="icon" href="favicon.png" type="image/png">
+    <link rel="stylesheet" type="text/css" href="/bower_components/bootstrap/dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="/bower_components/bootswatch/simplex/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="/css/login.css"/>
+    <script type="text/javascript" src="/bower_components/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <title>Log in - Minim</title>
+</head>
+<body>
+<div class="center-body">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <img src="/svg/logo.svg" class="logo">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <h3>Success!</h3>
+            <p>
+                You can only see this page if you're logged in. If you're seeing it now, congratulations! Minim is all
+                set up and configured. Remember there are some settings in <code>security.yml</code> that absolutely
+                <i>must</i> be changed before going to production.
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <hr>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <a class="btn btn-lg btn-info btn-block margin-bottom-plus" href="logout.php">Logout</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-muted text-center small copyright">
+            Minim | Copyright &copy; Saul Johnson
+        </div>
+    </div>
+</div>
+</body>
+</html>
