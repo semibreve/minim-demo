2c3b0c02216f584dba137117e897663dd22b7271
Saul Johnson <saul.a.johnson@gmail.com>
2020-03-06 18:11:07 +0000

    Fix reported vulnerabilities in NPM packages

--- gulpfile.js
+++ gulpfile.js
@@ -7,24 +7,24 @@ var gulp = require('gulp');
 // Less and CSS stuff.
 var less = require('gulp-less');
 var prefix = require('gulp-autoprefixer');
-var minifycss = require('gulp-minify-css');
+var cleancss = require('gulp-clean-css');
 
 // Compile all the Less.
 gulp.task('less', function () {
-    gulp.src(['./web/less/*.less'])
+    return gulp.src(['./web/less/*.less'])
         .pipe(less())
         .pipe(prefix(
             "last 1 version", "> 1%", "ie 8", "ie 7"
         ))
-        .pipe(minifycss()) // Minify resulting CSS.
+        .pipe(cleancss()) // Minify resulting CSS.
         .pipe(gulp.dest('./web/css'));
 });
 
 gulp.task('watch', function() {
     // Compile Less.
-    gulp.watch("./web/less/*.less", function(event) {
+    return gulp.watch("./web/less/*.less", function(event) {
         gulp.run('less');
     });
 });
 
-gulp.task('default', ['less']);
+gulp.task('default', gulp.series('less'));
